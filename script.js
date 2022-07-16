6e46155599c4a4af2fca738fc4b5c44e804a8e8d
Keanu2431 <lumpkinkeanu131@gmail.com>
2022-07-16 13:31:47 -0400

    Cleaned Up Unneccesary Code

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 `use strict`;
-
-// console.log(image.src);
-// image.src = imageArray[0];
-
-// const slideShow = function () {
-//   image.src = imageArray[i];
-//   if (i < imageArray.length - 1) i++;
-//   else i = 0;
-//   setTimeout(`slideShow()`, timeInterval);
-//   image.classList.remove(`fade-in`);
-//   image.classList.add(`fade-in`);
-// };
-
-// // slideShow();
-// window.onload = slideShow;
-
+//Slideshow
 window.addEventListener(
   "DOMContentLoaded",
   function (e) {
-    // Original JavaScript code by Chirp Internet: chirpinternet.eu
-    // Please acknowledge use of this code by including this header.
-
     const imageHolder = document.querySelector(`.image-holder`);
     var fadeComplete = function (e) {
       imageHolder.appendChild(arr[0]);
